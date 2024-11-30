92f0e8d0499fcb93a90eaa7253a3be01eaba4346
Felix Lehmann <felix_lehmann@ymail.com>
2024-11-30 18:36:38 +0000

    feat: add responsive burger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('scroll', function () {
     const navbar = document.getElementById("header_nav");
-    if (window.scrollY > 100) { // Wenn mehr als 50px gescrollt wurde
+    if (window.scrollY > 10) { // Wenn mehr als 50px gescrollt wurde
       let value = "#000000" + parseInt(Math.max(10, Math.min(255, window.scrollY / 2.7))).toString(16);
       console.log(value);
       navbar.style.backgroundColor = value;
