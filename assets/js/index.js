ef9cf245d8a76ed6307e5b0c259185499491a243
Felix Lehmann <felix_lehmann@ymail.com>
2024-11-30 17:56:11 +0000

    feat: finish dynamic opacity for the nav bar while scrolling

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,13 @@
 document.addEventListener('scroll', function () {
     const navbar = document.getElementById("header_nav");
-    if (window.scrollY > 50) { // Wenn mehr als 50px gescrollt wurde
-      navbar.classList.add('scrolled');
+    if (window.scrollY > 100) { // Wenn mehr als 50px gescrollt wurde
+      let value = "#000000" + parseInt(Math.max(10, Math.min(255, window.scrollY / 2.7))).toString(16);
+      console.log(value);
+      navbar.style.backgroundColor = value;
+      
+
     } else {
-      navbar.classList.remove('scrolled');
+      navbar.style.backgroundColor = "#00000000";
     }
   });
+
