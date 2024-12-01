aee310f69a766a978d51f3900ebe3e36c6f552b1
Felix Lehmann <felix_lehmann@ymail.com>
2024-12-01 17:35:10 +0000

    feat: add animation to burger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,7 +1,9 @@
+const menuIcon = document.getElementById('menuIcon');
+
 document.addEventListener('scroll', function () {
     const navbar = document.getElementById("header_nav");
     if (window.scrollY > 10) { // Wenn mehr als 50px gescrollt wurde
-      let value = "#000000" + parseInt(Math.max(10, Math.min(255, window.scrollY / 2.7))).toString(16);
+      let value = "#000000" + parseInt(Math.max(10, Math.min(255, window.scrollY / 1.9))).toString(16);
       console.log(value);
       navbar.style.backgroundColor = value;
       
@@ -10,4 +12,20 @@ document.addEventListener('scroll', function () {
       navbar.style.backgroundColor = "#00000000";
     }
   });
+
+menuIcon.addEventListener('click', function () {
+  if (menuIcon.classList.contains('fa-bars')) {
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-times');
+  } else {
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
+  }
+
+  // Füge eine Animation hinzu
+  menuIcon.classList.add('animate');
+
+  // Entferne die Animation nach der Dauer (300ms in diesem Fall)
+  setTimeout(() => menuIcon.classList.remove('animate'), 300);
+});
 
