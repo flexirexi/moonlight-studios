f48de74219efcf8630fa3fb1e674c51c613c9025
Felix Lehmann <felix_lehmann@ymail.com>
2024-12-01 21:49:53 +0000

    feat: add js to resize expanded nav menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,7 +2,59 @@ const menuIcon = document.getElementById('menuIcon');
 const nav = document.getElementById("header_nav");
 const nav_cont = document.getElementById("nav_right_container");
 const navbar = document.getElementById("header_nav");
+const nav_checkbox = document.getElementById("nav_dropdown");
 
+let wasSmallScreen = window.innerWidth < 768; // Initial prüfen, ob der Bildschirm klein ist
+
+
+function menuClick() {
+  if (menuIcon.classList.contains('fa-bars')) {
+    //öffnen
+    menuIcon.classList.remove('fa-bars');
+    menuIcon.classList.add('fa-times');
+    menuIcon.classList.add('animate'); // Drehung hinzufügen
+    
+    nav.style.backgroundColor = "black";
+    //nav.style.webkitBackdropFilter = 'blur(10px)'; //funzt irgendwie nicht - entweder nav oder nav_cont aber nicht beides...
+    
+    nav_cont.style.backdropFilter = "blur(10px)";
+    nav_cont.style.webkitBackdropFilter = 'blur(10px)';
+    
+    document.body.style.overflow = 'hidden'; //scrollen verbieten
+    nav_cont.style.overflowY = "auto"; //es gibt smartphones im querformat, bei denen das menu größer als der display ist..
+    document.body.style.overscrollBehavior = "none" //kein neu laden durch runterziehen
+    
+  } else {
+    //schließen
+    menuIcon.classList.remove('fa-times');
+    menuIcon.classList.add('fa-bars');
+    menuIcon.classList.remove('animate'); // Drehung entfernen
+    
+    //nav.style.backdropFilter = "none";
+    //nav.style.webkitBackdropFilter = 'none';
+    
+    
+    nav_cont.style.backdropFilter = "none";
+    nav_cont.style.webkitBackdropFilter = 'none';
+    
+    
+    document.body.style.overflow = ''; //scrollen erlauben
+    document.body.style.overscrollBehavior = "none" //kein neu laden durch runterziehen
+    nav_cont.style.overflowY = "hidden"; //rein pro forma wieder rückgängig machen..
+    
+    
+    
+    //anstatt die hintergrundfarbe ganz zu löschen, lieber in den zustand bringen, in den sie laut scroll position wäre:
+    let value = "#000000" + parseInt(Math.max(10, Math.min(255, window.scrollY / 1.9))).toString(16);
+    navbar.style.backgroundColor = value;
+  }
+  
+  // Füge eine Animation hinzu
+  menuIcon.classList.add('animate');
+  
+  // Entferne die Animation nach der Dauer (300ms in diesem Fall)
+  setTimeout(() => menuIcon.classList.remove('animate'), 300);
+}
 
 
 document.addEventListener('scroll', function () {
@@ -15,54 +67,7 @@ document.addEventListener('scroll', function () {
   }
 });
 
-menuIcon.addEventListener('click', function () {
-  if (menuIcon.classList.contains('fa-bars')) {
-    //öffnen
-    menuIcon.classList.remove('fa-bars');
-    menuIcon.classList.add('fa-times');
-    menuIcon.classList.add('animate'); // Drehung hinzufügen
-    
-    nav.style.backgroundColor = "black";
-    //nav.style.webkitBackdropFilter = 'blur(10px)'; //funzt irgendwie nicht - entweder nav oder nav_cont aber nicht beides...
-
-    nav_cont.style.backdropFilter = "blur(10px)";
-    nav_cont.style.webkitBackdropFilter = 'blur(10px)';
-
-    document.body.style.overflow = 'hidden'; //scrollen verbieten
-    nav_cont.style.overflowY = "auto"; //es gibt smartphones im querformat, bei denen das menu größer als der display ist..
-    document.body.style.overscrollBehavior = "none" //kein neu laden durch runterziehen
-
-  } else {
-    //schließen
-    menuIcon.classList.remove('fa-times');
-    menuIcon.classList.add('fa-bars');
-    menuIcon.classList.remove('animate'); // Drehung entfernen
-
-    //nav.style.backdropFilter = "none";
-    //nav.style.webkitBackdropFilter = 'none';
-
-
-    nav_cont.style.backdropFilter = "none";
-    nav_cont.style.webkitBackdropFilter = 'none';
-
-
-    document.body.style.overflow = ''; //scrollen erlauben
-    document.body.style.overscrollBehavior = "none" //kein neu laden durch runterziehen
-    nav_cont.style.overflowY = "hidden"; //rein pro forma wieder rückgängig machen..
- 
-
-
-    //anstatt die hintergrundfarbe ganz zu löschen, lieber in den zustand bringen, in den sie laut scroll position wäre:
-    let value = "#000000" + parseInt(Math.max(10, Math.min(255, window.scrollY / 1.9))).toString(16);
-    navbar.style.backgroundColor = value;
-  }
-
-  // Füge eine Animation hinzu
-  menuIcon.classList.add('animate');
-
-  // Entferne die Animation nach der Dauer (300ms in diesem Fall)
-  setTimeout(() => menuIcon.classList.remove('animate'), 300);
-});
+menuIcon.addEventListener('click', menuClick);
 
 document.addEventListener('touchmove', function (e) {
   // Prüfen, ob die Seite ganz oben ist und der Benutzer nach unten scrollt
@@ -70,3 +75,20 @@ document.addEventListener('touchmove', function (e) {
     e.preventDefault(); // Nur Pull-to-Refresh blockieren
   }
 }, { passive: false });
+
+
+window.addEventListener("resize", function () {
+  const isSmallScreen = window.innerWidth < 758;
+
+  // Prüfen, ob vorher kleiner als 768px und jetzt größer ist
+  if (wasSmallScreen && !isSmallScreen) {
+    if (menuIcon.classList.contains('fa-times')) {
+      console.log("Der Bildschirm ist jetzt größer als 768px!");
+      nav_checkbox.checked = false;
+      menuClick();
+    }
+  }
+
+  // Aktuellen Zustand speichern
+  wasSmallScreen = isSmallScreen;
+});
